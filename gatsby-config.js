6e7dd3f74095bf3ca120e1a5f1c518285b868afd
Adam <adamalexali@gmail.com>
2021-06-28 17:51:33 -0400

    added site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Adam Gatsby Site",
+    title: "Adam Ali — Digital Experience Designer, Prototyper, UX Researcher",
+    siteURL: "https://adamalexali.com",
+    description: "Adam Ali is a multidisciplinary experience designer exploring the intersections of technology, design, and society—specifically focused on digital public spaces, XR, accessibility, and tech for social good."
   },
   plugins: [],
 };
