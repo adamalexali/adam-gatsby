5a0afa9145ea9fb7a6c784490e0246a06e0626fa
Adam <adamalexali@gmail.com>
2021-07-02 18:12:18 -0400

    added styled-components to both pages

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,30 +2,33 @@ import * as React from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
 
+// styles
 const Container = styled.div`
+  font-family: -apple-system, Roboto, sans-serif, serif;
+  color: #333333;
   margin: 1rem auto;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
-// styles
-const footerStyles = {
-  fontFamily: '-apple-system, Roboto, sans-serif, serif',
-  fontSize: 12,
-  marginBottom: 32,
-};
 
+const FooterText = styled.p`
+  margin-bottom: 1rem;
+  font-size: 0.75rem;
+`;
+
+// component
 const Footer = () => {
   return (
     <Container>
-      <p style={footerStyles}>
+      <FooterText>
         Created with{' '}
         <span role='img' aria-label='heart emoji'>
           ❤️
         </span>{' '}
-        by Adam using <a href='https://www.gatsbyjs.com/'>Gatsby</a>
-      </p>
+        by Adam using <a href='https://www.gatsbyjs.com/'>Gatsby</a>&#33;
+      </FooterText>
     </Container>
   );
 };
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,27 +1,38 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
 import Footer from '../components/footer';
 
 // styles
-const pageStyles = {
-  color: '#232129',
-  padding: 96,
-  fontFamily: '-apple-system, Roboto, sans-serif, serif',
-};
-const headingStyles = {
-  marginTop: 16,
-  marginBottom: 32,
-  maxWidth: 320,
-};
-const paragraphStyles = {
-  marginBottom: 32,
-};
+const MainContent = styled('main')`
+  font-family: -apple-system, Roboto, sans-serif, serif;
+  color: #333333;
+  margin: 5rem 2rem 0 2rem;
+  height: 80vh;
+`;
+
+const Container = styled.div`
+  // margin: 1rem auto 0 auto;
+  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  justify-content: center;
+`;
+
+const HeadingText = styled.h1`
+  margin: 1rem 0;
+`;
+
+const Paragraph = styled.p`
+  margin-bottom: 1rem;
+`;
 
 const AboutPage = () => {
   return (
     <>
-      <main style={pageStyles}>
+      <MainContent>
         <Helmet>
           <meta charSet='utf-8' />
           <title>
@@ -29,31 +40,32 @@ const AboutPage = () => {
             About
           </title>
         </Helmet>
-        <Link to='/' style={paragraphStyles}>
-          Main page
-        </Link>
-        <h1 style={headingStyles}>Hi there!</h1>
-        <p style={paragraphStyles}>
-          My name's Adam. Since I was young, I’ve loved technology and design. I
-          grew up in the early days of the internet, and it had a huge influence
-          on me. I'm old enough to remember a time before our lives were
-          dominanted by the massive platforms of today... where the internet was
-          an exciting and uncharted field of discovery and information.
-        </p>
-        <p style={paragraphStyles}>
-          I want to be part of the community that examines and designs what the
-          future of digital connectivity is.
-        </p>
-        <p style={paragraphStyles}>
-          When I’m not working or in school, I like to spend my time playing
-          video games, watching anime, reading, running, or lifting weights. I’m
-          currently living &amp; working in the GTA, looking forward to life
-          after the pandemic{' '}
-          <span role='img' aria-label='Grinning emoji'>
-            😃
-          </span>
-        </p>
-      </main>
+        <Link to='/'>Main page</Link>
+        <Container>
+          <HeadingText>Hi there!</HeadingText>
+          <Paragraph>
+            My name's Adam. Since I was young, I’ve loved technology and design.
+            I grew up in the early days of the internet, and it had a huge
+            influence on me. I'm old enough to remember a time before our lives
+            were dominanted by the massive platforms of today... where the
+            internet was an exciting and uncharted field of discovery and
+            information.
+          </Paragraph>
+          <Paragraph>
+            I want to be part of the community that examines and designs what
+            the future of digital connectivity is.
+          </Paragraph>
+          <Paragraph>
+            When I’m not working or in school, I like to spend my time playing
+            video games, watching anime, reading, running, or lifting weights.
+            I’m currently living &amp; working in the GTA, looking forward to
+            life after the pandemic!{' '}
+            <span role='img' aria-label='Grinning emoji'>
+              😃
+            </span>
+          </Paragraph>
+        </Container>
+      </MainContent>
       <Footer />
     </>
   );
