fed8fc3a3dea659d9e45ce782ec1ddd0d4366aae
Adam <adamalexali@gmail.com>
2021-07-14 19:48:40 -0400

    removed reset.css ... please work

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
 import GlobalStyle from '../theme/globalStyle';
-import './reset.module.css';
 import styled from 'styled-components';
 
 // styles
